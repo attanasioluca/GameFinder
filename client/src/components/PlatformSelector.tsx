45b2fcf5ed985388e8e9faa41a03fe73951d917f
Luca Attanasio <91350993+attanasioluca@users.noreply.github.com>
2026-07-31 09:10:18 +0200

    Fix PlatformSelector missing return path

--- client/src/components/PlatformSelector.tsx
+++ client/src/components/PlatformSelector.tsx
@@ -9,38 +9,40 @@ import { BsChevronDown } from "react-icons/bs";
 import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
 interface Props {
-    onSelectedPlatform: (platform: Platform | null ) => void;
+    onSelectedPlatform: (platform: Platform | null) => void;
     selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({ onSelectedPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({
+    onSelectedPlatform,
+    selectedPlatform,
+}: Props) => {
     const { getPlatforms } = usePlatforms();
-    const {data, error} = getPlatforms();
-    if (error) return 
-    else if (data){
-        return (
-            <Menu>
-                <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-                    {selectedPlatform?.name || "Select Platform"}
-                </MenuButton>
-                <MenuList>
-                <MenuItem
-                            onClick={() => onSelectedPlatform(null)}
-                            key={"all"}
-                        >
-                            All platforms
-                        </MenuItem>
-                    {data.map((platform) => (
-                        <MenuItem
-                            onClick={() => onSelectedPlatform(platform)}
-                            key={platform.id}
-                        >
-                            {platform.name}
-                        </MenuItem>
-                    ))}
-                </MenuList>
-            </Menu>
-        );
-    }
+    const { data, error } = getPlatforms();
+
+    if (error) return null;
+    if (!data) return null;
+
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+                {selectedPlatform?.name || "Select Platform"}
+            </MenuButton>
+            <MenuList>
+                <MenuItem onClick={() => onSelectedPlatform(null)} key="all">
+                    All platforms
+                </MenuItem>
+                {data.map((platform) => (
+                    <MenuItem
+                        onClick={() => onSelectedPlatform(platform)}
+                        key={platform.id}
+                    >
+                        {platform.name}
+                    </MenuItem>
+                ))}
+            </MenuList>
+        </Menu>
+    );
 };
+
 export default PlatformSelector;
