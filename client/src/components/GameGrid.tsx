424afb12d69facb87ed6a86dd04243dcda2c4a45
Luca Attanasio <91350993+attanasioluca@users.noreply.github.com>
2026-07-31 09:10:28 +0200

    Fix GameGrid missing return path

--- client/src/components/GameGrid.tsx
+++ client/src/components/GameGrid.tsx
@@ -10,32 +10,36 @@ interface Props {
     onChange: (len: number) => void;
 }
 
-const GameGrid = ({ gameQuery, onChange}: Props) => {
+const GameGrid = ({ gameQuery, onChange }: Props) => {
     const { data, error, isLoading } = useGames(gameQuery);
-    
+
     const skeletons = [1, 2, 3, 4, 5, 6];
-    if (error) return <Text>{error}</Text>
-    if(data != null){
-        onChange(data.length)
-        return (
-            <SimpleGrid
-                columns={{ sm: 1, md: 2, lg: 3, xl: 5  }}
-                spacing={6}
-                paddingLeft={2}
-            >
-                {isLoading &&
-                    skeletons.map((skeleton) => (
-                        <GameCardContainer key={skeleton.toString()}>
-                            <GameCardSkeleton key={skeleton} />
-                        </GameCardContainer>
-                    ))}
-                {data.map((game) => (
-                    <GameCardContainer key={game.id} >
-                        <GameCard game={game}/>
+
+    if (error) return <Text>{error}</Text>;
+    if (!data) return null;
+
+    onChange(data.length);
+
+    return (
+        <SimpleGrid
+            columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+            spacing={6}
+            paddingLeft={2}
+        >
+            {isLoading &&
+                skeletons.map((skeleton) => (
+                    <GameCardContainer key={skeleton.toString()}>
+                        <GameCardSkeleton key={skeleton} />
                     </GameCardContainer>
                 ))}
-            </SimpleGrid>
-        );}
+
+            {data.map((game) => (
+                <GameCardContainer key={game.id}>
+                    <GameCard game={game} />
+                </GameCardContainer>
+            ))}
+        </SimpleGrid>
+    );
 };
 
 export default GameGrid;
